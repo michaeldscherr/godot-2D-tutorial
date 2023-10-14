5ec34619d283d6fceeb88412535e9b536e1d1eb6
Michael Scherr <mscherr@fastspot.com>
2023-10-14 09:37:38 -0400

    complets game

--- Main.gd
+++ Main.gd
@@ -6,11 +6,11 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -18,15 +18,29 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
+	$HUD.show_game_over()
+	
+	$Music.stop()
+	$DeathSound.play()
+	
 func new_game():
 	score = 0
 	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 	$Player.start($StartPosition.position)
 	
+	get_tree().call_group("mobs", "queue_free")
+	
 	$StartTimer.start()
+	
+	$Music.play()
 
 func _on_score_timer_timeout():
 	score += 1
+	
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
